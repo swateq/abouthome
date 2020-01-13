f923e14fad9b48ed350d77a1f9ec6d6a69d23ff1
swateq <swat94@tlen.pl>
2020-01-13 12:06:25 +0100

    Dodany tailwindcss

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>About Home</title>
+
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
